5dbf954eb84e77f027f67e04b9a7b77377239622
Bruna Bites <bruna.bites@gmail.com>
2022-03-07 16:20:28 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -46,17 +46,21 @@ if (typeof window !== 'undefined') {
 export default function App({ Component, pageProps }) {
   const router = useRouter()
 
-  useEffect(() => {
-    const handleRouteChange = url => {
-      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url,
-      })
+  React.useEffect(() => {
+    if (fathomId) {
+      Fathom.load(fathomId, fathomConfig)
+
+      function onRouteChangeComplete() {
+        Fathom.trackPageview()
+      }
+
+      router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+      return () => {
+        router.events.off('routeChangeComplete', onRouteChangeComplete)
+      }
     }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+  }, [])
 
   return <Component {...pageProps} />
 }
