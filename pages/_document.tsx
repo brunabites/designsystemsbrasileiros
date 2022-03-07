2ca0bcdb0fe741270a135dd0e4d6179d3d917027
Bruna Bites <bruna.bites@gmail.com>
2022-03-07 16:01:05 -0300

    Revert "Google Analytics setup"
    This reverts commit 66c9529de266afbf62e17178fa7413bf2f869d5b.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,23 +34,6 @@ export default class MyDocument extends Document {
               href='/favicon-16x16.png'
             />
 
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-            />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                page_path: window.location.pathname,
-              });
-            `,
-              }}
-            />
-
             <link rel='manifest' href='/manifest.json' />
           </Head>
 
